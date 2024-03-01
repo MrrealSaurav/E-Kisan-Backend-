c05d7a5e54fc19e91448dbf1360dcd39d7b68f28
Saurav <sssaurav010@gmail.com>
2024-03-01 17:50:48 +0530

    Backend updated
    E_Kisan Backend updated

---
+++ E-KISAN_backend/src/main/java/com/e_kisan/EKisanBackendApplication.java
@@ -0,0 +1,16 @@
+package com.e_kisan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EKisanBackendApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(EKisanBackendApplication.class, args);
+		
+		
+	}
+
+}
